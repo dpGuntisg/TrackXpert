cbed5e3de57a6f7a7028ff7d65eec0446423ae6d
dpguntisg <crazymangaming111@gmail.com>
2025-03-18 19:05:28 +0200

    .

--- frontend/src/components/UserContact.jsx
+++ frontend/src/components/UserContact.jsx
@@ -1,15 +1,13 @@
 // In UserContact component
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 export default function UserContact({ created_by }) {
-    if (!created_by) {
-        return <p>Loading user info...</p>;
-    }
-
     return (
-        <div>
+        <div className="gap-4 p-6 rounded-lg bg-gray-800 w-full hover:bg-gray-700 transition duration-300">
             <h3>Created by: {created_by.username}</h3>
-            <p>Email: {created_by.email}</p>
+            <p> <FontAwesomeIcon icon={faEnvelope}/> {created_by.email}</p>
         </div>
     );
 }
